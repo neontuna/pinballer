955419c040cc2b1a1aee6fa5b113c0dd6fb35d5d
Justin Mullis <justin.mullis@gmail.com>
2017-09-06 09:06:53 -0400

    added procfile, reassociate players whenever event is requested

--- app/controllers/matchplay_events_controller.rb
+++ app/controllers/matchplay_events_controller.rb
@@ -1,5 +1,6 @@
 class MatchplayEventsController < ApplicationController
   def show
     @event = MatchplayEvent.find_or_create_by(matchplay_id: params[:id])
+    @event.associate_players
   end
 end
